3bbe040052039d7773e593420e340b4a07edec69
vvk130 <vanessa.kasula@outlook.com>
2024-07-02 01:17:26 +0300

    Docker

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49008d9847e4654cc2cc9cfcbe9fe0a6ee93ff2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cb0ce129e8809da1c3fd057cb8a5d1b7b549612")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
