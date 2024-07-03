72c522c7f7324606dc52e20881deaeec63f70aa1
vvk130 <vanessa.kasula@outlook.com>
2024-07-03 03:04:19 +0300

    ownnotes

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4147c1381fe056923b996d7e646c656bb359508")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6df72bface3cea0435b3ae2ba8a4c3e5278cb30c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
